4ebda7724afd94c88b0cf3905003a85e6197ab4e
alvino.barboza <alvinobarboza@gmail.com>
2022-04-04 14:40:39 -0300

    First Commit

---
+++ app/src/main/java/com/youcast/settingsapp/MainFragment.java
@@ -0,0 +1,198 @@
+package com.youcast.settingsapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.leanback.app.BackgroundManager;
+import androidx.leanback.app.BrowseSupportFragment;
+import androidx.leanback.widget.ArrayObjectAdapter;
+import androidx.leanback.widget.HeaderItem;
+import androidx.leanback.widget.ListRow;
+import androidx.leanback.widget.ListRowPresenter;
+import androidx.leanback.widget.OnItemViewClickedListener;
+import androidx.leanback.widget.Presenter;
+import androidx.leanback.widget.Row;
+import androidx.leanback.widget.RowPresenter;
+import androidx.core.content.ContextCompat;
+
+import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainFragment extends BrowseSupportFragment {
+    private static final String TAG = "MainFragment";
+
+    private static final int BACKGROUND_UPDATE_DELAY = 300;
+    private static final int GRID_ITEM_WIDTH = 300 ;
+    private static final int GRID_ITEM_HEIGHT = 300;
+
+    private final Handler mHandler = new Handler();
+    private Drawable mDefaultBackground;
+    private DisplayMetrics mMetrics;
+    private Timer mBackgroundTimer;
+    private String mBackgroundUri;
+    private BackgroundManager mBackgroundManager;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onActivityCreated(savedInstanceState);
+        prepareBackgroundManager();
+        setupUIElements();
+        loadRows();
+        setupEventListeners();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (null != mBackgroundTimer) {
+            Log.d(TAG, "onDestroy: " + mBackgroundTimer.toString());
+            mBackgroundTimer.cancel();
+        }
+    }
+
+    private void loadRows() {
+        ListRowPresenter presenter = new ListRowPresenter();
+        //presenter.setNumRows(2);
+        ArrayObjectAdapter rowsAdapter = new ArrayObjectAdapter(presenter);
+
+        HeaderItem gridHeader = new HeaderItem(0, getString(R.string.preference));
+
+        GridItemPresenter mGridPresenter = new GridItemPresenter();
+        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
+
+        gridRowAdapter.add(getResources().getString(R.string.apps));
+        gridRowAdapter.add(getResources().getString(R.string.settings));
+        gridRowAdapter.add(getResources().getString(R.string.about));
+
+        rowsAdapter.add(new ListRow(gridHeader, gridRowAdapter));
+
+        setAdapter(rowsAdapter);
+    }
+
+    private void prepareBackgroundManager() {
+
+        mBackgroundManager = BackgroundManager.getInstance(getActivity());
+        mBackgroundManager.attach(getActivity().getWindow());
+
+        mDefaultBackground = ContextCompat.getDrawable(getContext(), R.drawable.default_background);
+        mMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
+    }
+
+    private void setupUIElements() {
+        // setBadgeDrawable(getActivity().getResources().getDrawable(
+        // R.drawable.videos_by_google_banner));
+        setTitle(getString(R.string.browse_title)); // Badge, when set, takes precedent
+        // over title
+        //setHeadersTransitionOnBackEnabled(true);
+        //setHeadersState(HEADERS_DISABLED);
+        // set fastLane (or headers) background color
+        //setBrandColor(ContextCompat.getColor(getContext(), R.color.fastlane_background));
+        // set search icon color
+        //setSearchAffordanceColor(ContextCompat.getColor(getContext(), R.color.search_opaque));
+    }
+
+    private void setupEventListeners() {
+        setOnItemViewClickedListener(new ItemViewClickedListener());
+    }
+
+    private void updateBackground(String uri) {
+        int width = mMetrics.widthPixels;
+        int height = mMetrics.heightPixels;
+        Glide.with(getActivity())
+                .load(uri)
+                .centerCrop()
+                .error(mDefaultBackground)
+                .into(new SimpleTarget<Drawable>(width, height) {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable drawable,
+                                                @Nullable Transition<? super Drawable> transition) {
+                        mBackgroundManager.setDrawable(drawable);
+                    }
+                });
+        mBackgroundTimer.cancel();
+    }
+
+    private void startBackgroundTimer() {
+        if (null != mBackgroundTimer) {
+            mBackgroundTimer.cancel();
+        }
+        mBackgroundTimer = new Timer();
+        mBackgroundTimer.schedule(new UpdateBackgroundTask(), BACKGROUND_UPDATE_DELAY);
+    }
+
+    private final class ItemViewClickedListener implements OnItemViewClickedListener {
+        @Override
+        public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item,
+                                  RowPresenter.ViewHolder rowViewHolder, Row row) {
+            if(((String) item).equals("Gerênciar aplicativos")){
+                startActivity( new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS));
+            }else if(((String) item).equals("Configurações")){
+                startActivity( new Intent(Settings.ACTION_SETTINGS));
+            }else if(((String) item).equals("Sobre")){
+                Intent intent = getActivity()
+                        .getPackageManager()
+                        .getLaunchIntentForPackage("com.changhong.systeminfo");
+                if(intent != null){
+                    startActivity(intent);
+                }else {
+                    startActivity(new Intent(Settings.ACTION_DEVICE_INFO_SETTINGS));
+                }
+            }
+        }
+    }
+
+    private class UpdateBackgroundTask extends TimerTask {
+        @Override
+        public void run() {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    updateBackground(mBackgroundUri);
+                }
+            });
+        }
+    }
+
+    private class GridItemPresenter extends Presenter {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent) {
+            TextView view = new TextView(parent.getContext());
+            view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
+            view.setFocusable(true);
+            view.setFocusableInTouchMode(true);
+            view.setBackgroundColor(
+                    ContextCompat.getColor(getContext(), R.color.default_background));
+            view.setTextColor(Color.WHITE);
+            view.setGravity(Gravity.CENTER);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+            ((TextView) viewHolder.view).setText((String) item);
+        }
+
+        @Override
+        public void onUnbindViewHolder(ViewHolder viewHolder) {
+        }
+    }
+
+}
